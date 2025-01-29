4f093cd8099c5a985b472c398a3a52a078520ac9
Martin Cornejo <martin.cornejo@tum.de>
2025-01-29 10:42:48 +0100

    Extend result analysis

--- optses/analysis.py
+++ optses/analysis.py
@@ -3,6 +3,45 @@ import pandas as pd
 import numpy as np
 
 
+def calc_fec(res):
+    return res["soc_sim"].diff().abs().sum() / 2
+
+
+def calc_roundtrip_efficiency(res):
+    p = res["power_sim"]
+    e_pos = p[p > 0].sum() * 0.25  # Wh
+    e_neg = p[p < 0].sum() * 0.25  # Wh
+
+    delta_soc = res["soc_sim"].iloc[-1] - res["soc_sim"].iloc[0]
+    delta_e = delta_soc * 66e3  # Wh
+
+    return np.abs(e_neg) / (e_pos - delta_e)
+
+
+def calc_revenue(res, price):
+    df = res.join(price)
+    return -1 * sum(df["power_sim"] * df["Intraday Continuous 15 minutes ID1-Price"]) * 0.25 * 1e-6  # W -> MWh
+
+
+def summary_results(results, price):
+    df = pd.DataFrame()
+    for name, res in results.items():
+        data = {"name": name, "FEC": calc_fec(res), "Eff": calc_roundtrip_efficiency(res), "Rev": calc_revenue(res, price)}
+        df = pd.concat([df, pd.DataFrame(data=[data])])
+    return df
+
+
+def print_performance(name, res, price):
+    print(f"-- {name} --")
+    fec = calc_fec(res)
+    eff = calc_roundtrip_efficiency(res)
+    rev = calc_revenue(res, price)
+    print(f"Full equivalent cycles: {fec:.1f}")
+    print(f"Roundtrip efficiency: {eff:.2%}")
+    print(f"Revenue: {rev:.2f}€")
+    print("")
+
+
 def plot_trading_power(res, price):
     fig, ax = plt.subplots(2, 2, height_ratios=[1, 3], width_ratios=[3, 1])
 
